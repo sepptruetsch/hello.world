604a546cbec38791127e5ac79deb43072e428935
sepptruetsch <AKIRA@Airrr.local>
2014-03-14 14:45:41 -0300

    init

---
+++ mongo.js
@@ -0,0 +1,19 @@
+{
+  // cache object
+  title: '',
+  body: '',
+  albums: [{
+  	// album object
+	id: '',
+	title: '',
+	items: [{
+		// item object
+		id: '',
+		description: '',
+		created: new Date()
+	}
+	],
+	created: new Date()
+  }],
+  created: new Date()
+}
